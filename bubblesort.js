d076ee280b38890169fa5a6ebb897bc6d7853dbf
Preethi-Kalyani <preethikalyani99@gmail.com>
2023-06-28 20:21:41 +0530

    optimized solution for bubble sort

--- bubblesort.js
+++ bubblesort.js
@@ -1,11 +1,14 @@
 function bubblesort(arr){
     const n = arr.length // storing total length of the array in a variable
-    for(let i = 0; i < n; i++){ 
-        for(let j = 0; j < (n - 1); j++){ // iterating all the values in the array
+    let isSwapped = false;
+    for(let i = 0; i < n && !isSwapped; i++){ 
+        isSwapped = true; 
+        for(let j = 0; j < (n - i); j++){ // iterating all the values in the array
             if(arr[j] > arr[j+1]){  // checking if the first number of the array is greater than the second one
                 let temp = arr[j];
                 arr[j] = arr[j+1];    // if so, swap the two numbers using temp variable 
                 arr[j+1] = temp; 
+                isSwapped = false; 
             }
         }
     }
